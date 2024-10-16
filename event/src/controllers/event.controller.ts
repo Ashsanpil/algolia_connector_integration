4c17f878b835c8991049415b41d899a1e5ed1ec9
Ashwin S Pillai <ashwin.pillai@ayatacommerce.com>
2024-10-16 12:13:52 +0530

    add custome errors-1

--- event/src/controllers/event.controller.ts
+++ event/src/controllers/event.controller.ts
@@ -1,56 +1,54 @@
 import { Request, Response } from 'express';
 import { createAlgoliaRecord } from '../services/product.service';
 import { saveProductToAlgolia } from '../client/algolia.client';
-import { 
-  NotFoundError, 
-  ValidationError, 
-  ExternalApiError 
-} from '../errors/custom.error';
+import CustomError from '../errors/custom.error';
 import { logger } from '../utils/logger.utils';
 
 export const post = async (request: Request, response: Response) => {
   try {
+    // Log when an event message is received
     logger.info('Received an event from Pub/Sub.');
 
-    // Validate that the message body exists
     if (!request.body || !request.body.message) {
-      throw new ValidationError('Invalid Pub/Sub message');
+      throw new CustomError(400, 'Bad request: Invalid Pub/Sub message');
     }
 
     const pubSubMessage = request.body.message;
+
+    // Decode the message
     const decodedData = pubSubMessage.data
       ? Buffer.from(pubSubMessage.data, 'base64').toString().trim()
       : undefined;
 
     if (!decodedData) {
-      throw new ValidationError('Empty message data');
+      throw new CustomError(400, 'Bad request: Empty message data');
     }
 
     const jsonData = JSON.parse(decodedData);
-    const productId = jsonData.productProjection?.id;
-
-    if (!productId) {
-      throw new NotFoundError('Product', 'unknown');
-    }
+    const productId = jsonData.productProjection.id;
 
     // Log product ID to confirm successful data extraction
     logger.info(`Processing product with ID: ${productId}`);
 
-    // Fetch and index product
+    // Create Algolia record from product data
     const algoliaRecord = await createAlgoliaRecord(productId);
+
+    // Save product to Algolia
     await saveProductToAlgolia(algoliaRecord);
 
+    // Log successful indexing in Algolia
     logger.info(`Product ${productId} successfully indexed in Algolia`);
+
+    // Send a response to acknowledge the request
     response.status(204).send();
   } catch (error) {
-    if (error instanceof ValidationError || error instanceof NotFoundError) {
-      response.status(Number(error.statusCode)).json({ error: error.message });
-    } else if (error instanceof ExternalApiError) {
-      logger.error(`Error from external service: ${error.message}`);
-      response.status(Number(error.statusCode)).json({ error: error.message });
-    } else {
-      logger.error('Internal server error:', error);
-      response.status(500).json({ error: 'Internal Server Error' });
+    // Log any errors encountered during processing
+    logger.error('Error processing product:', error);
+
+    if (error instanceof CustomError) {
+      throw error;
     }
+
+    throw new CustomError(500, `Error processing product: ${error}`);
   }
 };
